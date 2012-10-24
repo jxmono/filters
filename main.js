ab40336e1f602b20d4d840b1533a3fd21af03480
Gabriel Petrovay <gabipetrovay@gmail.com>
2012-10-24 16:14:59 +0200

    a prototype version of the filtering module

--- main.js
+++ main.js
@@ -1,12 +1,43 @@
 define(function() {
 
+    var self = null;
+
     var filter = {};
 
-    this.mergeFilter = function(data) {
-        debugger;
+    function mergeCategoryFilter(data) {
+        filter.category = data.id;
+        self.emit("filterChanged", filter);
+    }
+
+    function mergeSearchFilter(data) {
+        // TODO
     }
 
     function init(config) {
+
+        self = this;
+
+        self.mergeCategoryFilter = mergeCategoryFilter;
+
+        // for each module instance I listen to
+        for (var miid in config.listen) {
+            var miidEvents = config.listen[miid];
+            // for each event for this instance
+            for (var name in miidEvents) {
+                var handler = miidEvents[name];
+                // if the handler is a module function name
+                if (typeof handler === "string" && typeof self[handler] === "function") {
+                    self.on(name, miid, function(data) {
+                        self[handler].call(self, data);
+                    });
+                    continue;
+                }
+                // else it must be object
+                if (handler instanceof Object) {
+                    // TODO
+                }
+            }
+        }
     }
 
     return init;
