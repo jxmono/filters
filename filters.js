6e31069fc2e60868ecaf7932f53a4579fe570b22
Gabriel Petrovay <gabipetrovay@gmail.com>
2013-06-13 12:49:22 +0200

    hardcoded also the field to search when a 'text' search field is changed: searching in description

--- filters.js
+++ filters.js
@@ -1,4 +1,4 @@
-var Events = require('github/jillix/events');
+var Events = require("github/jillix/events");
 
 module.exports = function (config) {
 
@@ -7,29 +7,36 @@ module.exports = function (config) {
 
     function mergeCategoryFilter (data) {
         filter.category = data ? data.id : undefined;
-        self.emit('filterChanged', filter);
+        self.emit("filterChanged", filter);
     }
 
     function mergeSearchFilter (data) {
 
         // TODO move hardcoded filter keys to configuration
-        delete filter['type'];
+        delete filter["type"];
 
         for (var i in data) {
             switch (i) {
-                case 'priceRange':
+                case "priceRange":
                     // priceRange is a 2-element array
                     filter.price = {
                         $gte: data[i][0],
                         $lte: data[i][1]
                     }
                     break;
-                case 'type':
+                case "type":
                     filter.type = data.type;
+                    break;
+                case "text":
+                    if (!data.text) {
+                        delete filter.description;
+                    } else {
+                        filter.description = { "$regex" : ".*" + data.text.split(" ").join(".*") + ".*"} ;
+                    }
                     break;
             }
         }
-        self.emit('filterChanged', filter);
+        self.emit("filterChanged", filter);
     }
 
     self.mergeCategoryFilter = mergeCategoryFilter;
